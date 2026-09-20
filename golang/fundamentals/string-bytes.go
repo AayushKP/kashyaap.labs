7c2dae7ab9711a6a5eae6d1c6ba965e33eabef8e
AayushKP <ayushprokashyap@gmail.com>
2026-09-20 21:28:24 +0530

    strings in go

---
+++ golang/fundamentals/string-bytes.go
@@ -0,0 +1,13 @@
+/*
+ * name := "Aayush"
+ * fmt.Println(name[0])
+ * It prints the byte value: 65
+ * Because a Go string is fundamentally a sequence of bytes.
+ * len() gives bytes
+ *
+ * English:
+
+ A
+ ↓
+ 1 byte
+ */
